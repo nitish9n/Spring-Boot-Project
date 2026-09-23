fc0fee4e6f8d0e999f302f0af373b81beb524b9a
aarav <aaravaryan24@gmail.com>
2026-09-23 21:07:07 +0530

    Initial commit

---
+++ src/test/java/com/learning/boot/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
